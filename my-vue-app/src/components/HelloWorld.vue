7abdb8fe7a0620604826f4332c281f213cc10e24
Charliemiz <cdmisbach@icloud.com>
2025-02-22 15:13:52 -0600

    application environment created and tailwindcss implemented for styling

---
+++ my-vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card text-3xl font-bold flex flex-col">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
