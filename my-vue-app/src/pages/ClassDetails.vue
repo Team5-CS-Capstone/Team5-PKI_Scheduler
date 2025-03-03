e521173427638124fa3df7a35163d332c83235a3
Charliemiz <cdmisbach@icloud.com>
2025-03-03 12:37:21 -0600

    start of class details page

--- my-vue-app/src/pages/ClassDetails.vue
+++ my-vue-app/src/pages/ClassDetails.vue
@@ -1,2 +1,16 @@
+<template>
+    <span class=" text-black text-2xl font-semibold mb-4 text-start">CSCI {}</span>
+    <div class="flex flex-col space-y-4 text-black text-start h-full">
+        <span>Term: {{ placeholder }}</span>
+        <span>Section: {{ placeholder }}</span>
+        <span>Instructor:</span>
+        <span>Room: </span>
+        <span>Time: </span>
+        <span>Current Enrollment: </span>
+        <span>Max Enrollment: </span>
+    </div>
+</template>
 
-<template><span>Class Details</span></template>
+<script>
+
+</script>
