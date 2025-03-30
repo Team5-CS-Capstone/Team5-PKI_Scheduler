b171ce8d5d6a75a4dcbb018ba3b6f07478f1a33e
Charliemiz <cdmisbach@icloud.com>
2025-03-30 18:18:32 -0500

    t
    reset

--- python-db/sphinx_output/conf.py
+++ python-db/sphinx_output/conf.py
@@ -35,3 +35,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+html_baseurl = 'https://charliemiz.github.io/Team5-PKI_Scheduler/'
+
