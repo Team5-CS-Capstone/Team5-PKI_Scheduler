9b3be499cffec4bd380c55943d606981d326cdce
Charliemiz <cdmisbach@icloud.com>
2025-03-31 11:34:49 -0500

    added pages to about, updated requirements, moved docs to root level

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../python-db'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -13,27 +13,19 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'PKI Scheduler - Team 5'
 copyright = '2025, Charlie, Zaid, Brian, John, Brady'
 author = 'Charlie, Zaid, Brian, John, Brady'
-release = '0.1'
+release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx.ext.viewcode',
-    'sphinx.ext.todo',
+    'sphinx.ext.napoleon',
     'sphinx.ext.autodoc',
 ]
 todo_include_todos = True
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_baseurl = 'https://charliemiz.github.io/Team5-PKI_Scheduler/'
-
